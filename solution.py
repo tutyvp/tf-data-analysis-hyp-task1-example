ee225ff37053de437e70614fc13aeb95bdc41ac9
tutyvp <126392885+tutyvp@users.noreply.github.com>
2023-04-13 19:35:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,27 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 460411293 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control_sales, control_apps, test_sales, test_apps, alpha=0.04):
+  
+  control_conversion = control_sales / control_apps
+
+ 
+  expected_sales = control_conversion * test_apps
+
+  
+  std_dev = math.sqrt(expected_sales * (1 - control_conversion))
+
+ 
+  z_score = (test_sales - expected_sales) / std_dev
+
+  
+  critical_value = abs(stats.norm.ppf(alpha / 2))
+
+  
+  if abs(z_score) > critical_value:
+    return True # Reject the null hypothesis
+  else:
+    return False # Fail to reject the null hypothesis
+
